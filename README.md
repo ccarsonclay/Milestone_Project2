d9d56a2a1bb702cee158d4d9a0c2f03be78c10f4
ccarsonclay <carsonaclay@gmail.com>
2023-05-06 14:21:47 -0400

    finishing Mern

---
+++ README.md
@@ -0,0 +1,7 @@
+todo-list Milesstone Project-2 
+Written By
+Carson Clay
+Jonphia Hart
+Gavin 
+Austin James
+
--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,78 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./style.css";
 
 export const App = () => {
+	const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleCreateTask = (e) => {
+	e.preventDefault();
+	console.log(e.target.value);
+    setTask(e.target.value);
+  }
+
+  const handleAddTask = () => {
+	setTaskItems([...taskItems, task]);
+	console.log(taskItems)
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy)
+
+
+  }
+
+  
+  console.log(taskItems);
 	return (
-		<h1>App component</h1>
-	)
-}
-// window.addEventListener('load', () => {
-// 	const form = document.querySelector("#new-task-form");
-// 	const input = document.querySelector("#new-task-input");
-// 	const list_el = document.querySelector("#tasks");
+		<div>
+		<h1>Task List 2023</h1>
+		<form id="new-task-form">
+			<input 
+				type="text" 
+				name="new-task-input" 
+				id="new-task-input" 
+				onChange={ e => handleCreateTask(e) }
+				placeholder="What do you have planned?" />
+			<input 
+				type="button"
+				id="new-task-submit" 
+				value="submit"
+				onClick={ () => handleAddTask() }
+				/>
+		</form>
+	<main>
+		<section className="task-list">
+			<h2>Tasks</h2>
 
-// 	form.addEventListener('submit', (e) => {
-// 		e.preventDefault();
+			<div id="tasks">
+				<ul>
+					{ taskItems.map(task => (
+						<li>{ task }</li>
+				))}
 
-// 		const task = input.value;
+				</ul>
 
-// 		const task_el = document.createElement('div');
-// 		task_el.classList.add('task');
+				 <div className="task">
+					<div className="content">
+						<input 
+							type="text" 
+							className="text" 
+							defaultValue=""
+							 />
+					</div>
+					<div className="actions">
+						<button className="edit">Edit</button>
+						<button className="delete">Delete</button>
+					</div>
+				</div> 
 
-// 		const task_content_el = document.createElement('div');
-// 		task_content_el.classList.add('content');
-
-// 		task_el.appendChild(task_content_el);
-
-// 		const task_input_el = document.createElement('input');
-// 		task_input_el.classList.add('text');
-// 		task_input_el.type = 'text';
-// 		task_input_el.value = task;
-// 		task_input_el.setAttribute('readonly', 'readonly');
-
-// 		task_content_el.appendChild(task_input_el);
-
-// 		const task_actions_el = document.createElement('div');
-// 		task_actions_el.classList.add('actions');
+			</div>
+		</section>
 		
-// 		const task_edit_el = document.createElement('button');
-// 		task_edit_el.classList.add('edit');
-// 		task_edit_el.innerText = 'Edit';
-
-// 		const task_delete_el = document.createElement('button');
-// 		task_delete_el.classList.add('delete');
-// 		task_delete_el.innerText = 'Delete';
-
-// 		task_actions_el.appendChild(task_edit_el);
-// 		task_actions_el.appendChild(task_delete_el);
-
-// 		task_el.appendChild(task_actions_el);
-
-// 		list_el.appendChild(task_el);
-
-// 		input.value = '';
-
-// 		task_edit_el.addEventListener('click', (e) => {
-// 			if (task_edit_el.innerText.toLowerCase() == "edit") {
-// 				task_edit_el.innerText = "Save";
-// 				task_input_el.removeAttribute("readonly");
-// 				task_input_el.focus();
-// 			} else {
-// 				task_edit_el.innerText = "Edit";
-// 				task_input_el.setAttribute("readonly", "readonly");
-// 			}
-// 		});
-
-// 		task_delete_el.addEventListener('click', (e) => {
-// 			list_el.removeChild(task_el);
-// 		});
-// 	});
-// });
+	</main>
+	</div>
+	)
+}	
